88bd94b99b0980aa88e3788cb4a2c4f9bca1b974
Alex Lin <alexlinpm@gmail.com>
2020-04-04 11:54:01 -0700

    use webpack instead of rollup

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/index.html', './src/**/*.svelte'],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+
+  whitelistPatterns: [/svelte-/],
+})
+
+const prod = process.env.NODE_ENV === 'production'
+const plugins = prod ? [purgecss] : []
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...plugins,
+  ],
+}
