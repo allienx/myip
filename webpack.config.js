2e047aaa7233588ea3c239013c0af1a863f170f2
Alex Lin <alexlinpm@gmail.com>
2020-04-04 12:40:00 -0700

    Feature - use webpack and tailwind (#1)
    * use webpack instead of rollup
    
    * add prettier
    
    * use tailwind styling

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const sveltePreprocess = require('svelte-preprocess')
+
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
+
+const outputDir = path.join(__dirname, 'dist')
+
+module.exports = {
+  mode,
+  devtool: prod ? false : 'source-map',
+  entry: {
+    main: ['./src/main.js'],
+  },
+  output: {
+    path: outputDir,
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[id].[contenthash].js',
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte'),
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true,
+            preprocess: sveltePreprocess({
+              postcss: true,
+            }),
+          },
+        },
+      },
+      {
+        test: /\.(css|pcss)$/,
+        use: [
+          // MiniCssExtractPlugin doesn't support HMR.
+          // For developing, use 'style-loader' instead.
+          prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: outputDir,
+    compress: true,
+    port: 1234,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{ from: './src/favicon' }]),
+    new HtmlWebpackPlugin({
+      minify: prod,
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[id].[contenthash].css',
+    }),
+  ],
+}
