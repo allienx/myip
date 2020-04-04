6c78d8596e3a62bd0bae0ae965ed5fdcf1d95915
Alex Lin <alexlinpm@gmail.com>
2020-04-04 12:28:53 -0700

    use tailwind styling

--- src/App.svelte
+++ src/App.svelte
@@ -11,44 +11,78 @@
   })
 </script>
 
+<style>
+  main {
+    @apply min-h-screen max-w-screen-sm mx-auto flex items-center;
+  }
+
+  h1 {
+    @apply text-5xl font-bold text-center mb-16;
+  }
+
+  h2 {
+    @apply text-2xl font-bold text-center my-8;
+  }
+
+  a {
+    color: #0076ff;
+    border-bottom: 1px solid white;
+    transition: all 0.2s ease;
+  }
+
+  a:hover {
+    border-bottom: 1px solid #0076ff;
+  }
+
+  p {
+    @apply text-center;
+  }
+
+  code {
+    color: #d400ff;
+  }
+</style>
+
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
-      ZEIT Now
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/zeit/now/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  {#if isLoading}
-    <p>Loading date...</p>
-  {:else}
-    <p>{date}</p>
-  {/if}
+  <div>
+    <h1>Svelte + Node.js API</h1>
+    <h2>
+      Deployed with
+      <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
+        ZEIT Now
+      </a>
+      !
+    </h2>
+    <p>
+      <a
+        href="https://github.com/allienx/myip"
+        target="_blank"
+        rel="noreferrer noopener">
+        This project
+      </a>
+      is a
+      <a href="https://svelte.dev/">Svelte</a>
+      app with three directories,
+      <code>/public</code>
+      for static assets,
+      <code>/src</code>
+      for components and content, and
+      <code>/api</code>
+      which contains a serverless
+      <a href="https://nodejs.org/en/">Node.js</a>
+      function. See
+      <a href="/api/date">
+        <code>api/date</code>
+        for the Date API with Node.js
+      </a>
+      .
+    </p>
+    <br />
+    <h2>The date according to Node.js is:</h2>
+    {#if isLoading}
+      <p>Loading date...</p>
+    {:else}
+      <p>{date}</p>
+    {/if}
+  </div>
 </main>
